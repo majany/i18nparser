1beb4042818181a14b06e7ecf6e25380b107e337
majany <majany94@gmail.com>
2019-08-28 20:21:11 +0200

    wip class

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const fs = require("fs");
 
 class I18NPropertiesFile {
 
-    i18nParser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-    parserDefs = new nearley.Parser(nearley.Grammar.fromCompiled(assdefgrammar))
-    mLines = []
-    mProperties = {}
-
     constructor() {
+        this.i18nParser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+        this.parserDefs = new nearley.Parser(nearley.Grammar.fromCompiled(assdefgrammar));
         this.mStartState = this.i18nParser.save();
         this.mStartStateDef = this.parserDefs.save();
+        this.mLines = [];
+        this.mProperties = {};
     }
 
     addFile(sI18nFilePath) {
@@ -25,11 +24,16 @@ class I18NPropertiesFile {
         if (this.i18nParser.results.length > 1) {
             throw new Error("Fatal Error: Grammar is ambigious!");
         }
-        let result = this.i18nParser.results[0];
+        this.mLines = this._getPostProcessedParserResult(this.i18nParser.results);
+        this._addToPropertiesBag(this.mLines);
+    }
+
+    _getPostProcessedParserResult(results){
+        let result = results[0];
         let lines = result[0].slice();
         lines.push(result[1]); // add last line
-        this.mLines = this._parseDefinitions(lines);
-        this._addToPropertiesBag(this.mLines);
+        this._parseDefinitions(lines);
+        return lines;
     }
 
     _parseDefinitions(lines){
@@ -55,13 +59,14 @@ class I18NPropertiesFile {
     }
 
     _addToPropertiesBag(lines) {
-        this.mLines.forEach( (line, index) => {
+        lines.forEach( (line, index) => {
             if(line.lineType === "assignment"){
                 this.mProperties[line.key] = {
                     text: line.text,
                     line: index,
                     fileName: "TODO: insert name"
                 };
+                // TODO: check line before for definition
             }
         });
     }
