fa661bb19bca7a8643ba82cd69bc33e5246149e5
Mathaeus Jany <mathaeus.jany@sap.com>
2019-08-28 15:12:34 +0200

    added assignmentdef

--- grammar.js
+++ grammar.js
@@ -27,35 +27,134 @@ function $(o) {
 
 function commentType(d){
     return {
-        type: "comment",
+        lineType: "comment",
         text: d[1]
     }
 }
 function assignmentType(d){
     return {
-        type: "assignment",
+        lineType: "assignment",
         key: d[0],
         text: d[2]
+    }
+}
+function assingmentdefType(d){
+    return {
+        lineType: "assignmentdef",
+        type: d[1],
+        length: d[2],
+        text: d[4]
     }
 }
 var grammar = {
     Lexer: undefined,
     ParserRules: [
+    {"name": "unsigned_int$ebnf$1", "symbols": [/[0-9]/]},
+    {"name": "unsigned_int$ebnf$1", "symbols": ["unsigned_int$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "unsigned_int", "symbols": ["unsigned_int$ebnf$1"], "postprocess": 
+        function(d) {
+            return parseInt(d[0].join(""));
+        }
+        },
+    {"name": "int$ebnf$1$subexpression$1", "symbols": [{"literal":"-"}]},
+    {"name": "int$ebnf$1$subexpression$1", "symbols": [{"literal":"+"}]},
+    {"name": "int$ebnf$1", "symbols": ["int$ebnf$1$subexpression$1"], "postprocess": id},
+    {"name": "int$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "int$ebnf$2", "symbols": [/[0-9]/]},
+    {"name": "int$ebnf$2", "symbols": ["int$ebnf$2", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "int", "symbols": ["int$ebnf$1", "int$ebnf$2"], "postprocess": 
+        function(d) {
+            if (d[0]) {
+                return parseInt(d[0][0]+d[1].join(""));
+            } else {
+                return parseInt(d[1].join(""));
+            }
+        }
+        },
+    {"name": "unsigned_decimal$ebnf$1", "symbols": [/[0-9]/]},
+    {"name": "unsigned_decimal$ebnf$1", "symbols": ["unsigned_decimal$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "unsigned_decimal$ebnf$2$subexpression$1$ebnf$1", "symbols": [/[0-9]/]},
+    {"name": "unsigned_decimal$ebnf$2$subexpression$1$ebnf$1", "symbols": ["unsigned_decimal$ebnf$2$subexpression$1$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "unsigned_decimal$ebnf$2$subexpression$1", "symbols": [{"literal":"."}, "unsigned_decimal$ebnf$2$subexpression$1$ebnf$1"]},
+    {"name": "unsigned_decimal$ebnf$2", "symbols": ["unsigned_decimal$ebnf$2$subexpression$1"], "postprocess": id},
+    {"name": "unsigned_decimal$ebnf$2", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "unsigned_decimal", "symbols": ["unsigned_decimal$ebnf$1", "unsigned_decimal$ebnf$2"], "postprocess": 
+        function(d) {
+            return parseFloat(
+                d[0].join("") +
+                (d[1] ? "."+d[1][1].join("") : "")
+            );
+        }
+        },
+    {"name": "decimal$ebnf$1", "symbols": [{"literal":"-"}], "postprocess": id},
+    {"name": "decimal$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "decimal$ebnf$2", "symbols": [/[0-9]/]},
+    {"name": "decimal$ebnf$2", "symbols": ["decimal$ebnf$2", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "decimal$ebnf$3$subexpression$1$ebnf$1", "symbols": [/[0-9]/]},
+    {"name": "decimal$ebnf$3$subexpression$1$ebnf$1", "symbols": ["decimal$ebnf$3$subexpression$1$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "decimal$ebnf$3$subexpression$1", "symbols": [{"literal":"."}, "decimal$ebnf$3$subexpression$1$ebnf$1"]},
+    {"name": "decimal$ebnf$3", "symbols": ["decimal$ebnf$3$subexpression$1"], "postprocess": id},
+    {"name": "decimal$ebnf$3", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "decimal", "symbols": ["decimal$ebnf$1", "decimal$ebnf$2", "decimal$ebnf$3"], "postprocess": 
+        function(d) {
+            return parseFloat(
+                (d[0] || "") +
+                d[1].join("") +
+                (d[2] ? "."+d[2][1].join("") : "")
+            );
+        }
+        },
+    {"name": "percentage", "symbols": ["decimal", {"literal":"%"}], "postprocess": 
+        function(d) {
+            return d[0]/100;
+        }
+        },
+    {"name": "jsonfloat$ebnf$1", "symbols": [{"literal":"-"}], "postprocess": id},
+    {"name": "jsonfloat$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "jsonfloat$ebnf$2", "symbols": [/[0-9]/]},
+    {"name": "jsonfloat$ebnf$2", "symbols": ["jsonfloat$ebnf$2", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "jsonfloat$ebnf$3$subexpression$1$ebnf$1", "symbols": [/[0-9]/]},
+    {"name": "jsonfloat$ebnf$3$subexpression$1$ebnf$1", "symbols": ["jsonfloat$ebnf$3$subexpression$1$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "jsonfloat$ebnf$3$subexpression$1", "symbols": [{"literal":"."}, "jsonfloat$ebnf$3$subexpression$1$ebnf$1"]},
+    {"name": "jsonfloat$ebnf$3", "symbols": ["jsonfloat$ebnf$3$subexpression$1"], "postprocess": id},
+    {"name": "jsonfloat$ebnf$3", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "jsonfloat$ebnf$4$subexpression$1$ebnf$1", "symbols": [/[+-]/], "postprocess": id},
+    {"name": "jsonfloat$ebnf$4$subexpression$1$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "jsonfloat$ebnf$4$subexpression$1$ebnf$2", "symbols": [/[0-9]/]},
+    {"name": "jsonfloat$ebnf$4$subexpression$1$ebnf$2", "symbols": ["jsonfloat$ebnf$4$subexpression$1$ebnf$2", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "jsonfloat$ebnf$4$subexpression$1", "symbols": [/[eE]/, "jsonfloat$ebnf$4$subexpression$1$ebnf$1", "jsonfloat$ebnf$4$subexpression$1$ebnf$2"]},
+    {"name": "jsonfloat$ebnf$4", "symbols": ["jsonfloat$ebnf$4$subexpression$1"], "postprocess": id},
+    {"name": "jsonfloat$ebnf$4", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "jsonfloat", "symbols": ["jsonfloat$ebnf$1", "jsonfloat$ebnf$2", "jsonfloat$ebnf$3", "jsonfloat$ebnf$4"], "postprocess": 
+        function(d) {
+            return parseFloat(
+                (d[0] || "") +
+                d[1].join("") +
+                (d[2] ? "."+d[2][1].join("") : "") +
+                (d[3] ? "e" + (d[3][1] || "+") + d[3][2].join("") : "")
+            );
+        }
+        },
     {"name": "properties$ebnf$1", "symbols": []},
     {"name": "properties$ebnf$1$subexpression$1", "symbols": ["statement", {"literal":"\n"}]},
     {"name": "properties$ebnf$1", "symbols": ["properties$ebnf$1", "properties$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "properties", "symbols": ["properties$ebnf$1", "statement"]},
-    {"name": "statement", "symbols": ["assignment"]},
+    {"name": "statement", "symbols": ["assignment"], "postprocess": id},
+    {"name": "statement", "symbols": ["assignmentdef"], "postprocess": id},
     {"name": "statement", "symbols": ["_", "comment"], "postprocess": commentType},
     {"name": "statement", "symbols": ["_"], "postprocess": () => null},
     {"name": "assignment$ebnf$1", "symbols": ["comment"], "postprocess": id},
     {"name": "assignment$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "assignment", "symbols": ["word", {"literal":"="}, "transText", "assignment$ebnf$1"], "postprocess": assignmentType},
-    {"name": "comment", "symbols": [{"literal":"#"}, "text"], "postprocess": d => d[0] + d[1].join("")},
+    {"name": "assignment", "symbols": ["key", {"literal":"="}, "transText", "assignment$ebnf$1"], "postprocess": assignmentType},
+    {"name": "assignmentdef$ebnf$1", "symbols": ["lenghtDef"], "postprocess": id},
+    {"name": "assignmentdef$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "assignmentdef", "symbols": [{"literal":"#"}, "type", "assignmentdef$ebnf$1", {"literal":":"}, "text"], "postprocess": assingmentdefType},
+    {"name": "lenghtDef", "symbols": [{"literal":","}, "_", "unsigned_int", "_"], "postprocess": nth(2)},
+    {"name": "comment", "symbols": [{"literal":"#"}, "text"], "postprocess": d => d[0] + d[1]},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": function(d) {return null;}},
-    {"name": "wschar", "symbols": [/[ \t\v\f]/], "postprocess": id},
+    {"name": "wschar", "symbols": [/[ \t]/], "postprocess": id},
     {"name": "transText", "symbols": ["transChar", "transRest"], "postprocess": d => d[0] + d[1].join("")},
     {"name": "transRest$ebnf$1", "symbols": []},
     {"name": "transRest$ebnf$1", "symbols": ["transRest$ebnf$1", "transChar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
@@ -63,12 +162,16 @@ var grammar = {
     {"name": "transChar", "symbols": [/[^\n\t\v\f#]/], "postprocess": id},
     {"name": "text$ebnf$1", "symbols": []},
     {"name": "text$ebnf$1", "symbols": ["text$ebnf$1", "textChar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "text", "symbols": ["text$ebnf$1"], "postprocess": id},
+    {"name": "text", "symbols": ["text$ebnf$1"], "postprocess": d => d[0].join("")},
     {"name": "textChar", "symbols": [/[^\n\v\f]/], "postprocess": id},
-    {"name": "word$ebnf$1", "symbols": ["char"]},
-    {"name": "word$ebnf$1", "symbols": ["word$ebnf$1", "char"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "word", "symbols": ["word$ebnf$1"], "postprocess": d => d[0].join("")},
-    {"name": "char", "symbols": [/[^\n\t\v\f ]/], "postprocess": id}
+    {"name": "key$ebnf$1", "symbols": ["char"]},
+    {"name": "key$ebnf$1", "symbols": ["key$ebnf$1", "char"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "key", "symbols": ["key$ebnf$1"], "postprocess": d => d[0].join("")},
+    {"name": "char", "symbols": [/[^\n\t\v\f ]/], "postprocess": id},
+    {"name": "type$string$1", "symbols": [{"literal":"X"}, {"literal":"B"}, {"literal":"U"}, {"literal":"T"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "type", "symbols": ["type$string$1"]},
+    {"name": "type$string$2", "symbols": [{"literal":"X"}, {"literal":"F"}, {"literal":"L"}, {"literal":"D"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "type", "symbols": ["type$string$2"]}
 ]
   , ParserStart: "properties"
 }
--- grammar.ne
+++ grammar.ne
@@ -1,37 +1,52 @@
 @builtin "postprocessors.ne"
+@builtin "number.ne"
 @{%
 function commentType(d){
     return {
-        type: "comment",
+        lineType: "comment",
         text: d[1]
     }
 }
 function assignmentType(d){
     return {
-        type: "assignment",
+        lineType: "assignment",
         key: d[0],
         text: d[2]
     }
 }
+function assingmentdefType(d){
+    return {
+        lineType: "assignmentdef",
+        type: d[1],
+        length: d[2],
+        text: d[4]
+    }
+}
 %}
 
-properties -> (statement "\n"):* statement 
-statement -> assignment 
+properties -> (statement "\n"):* statement
+statement -> assignment {% id %}
+           | assignmentdef {% id %}
             | _ comment {% commentType %}
             | _ {% () => null %}
 
-assignment -> word "=" transText comment:? {% assignmentType %}
-comment -> "#" text {% d => d[0] + d[1].join("") %}
+assignment -> key "=" transText comment:? {% assignmentType %}
+assignmentdef -> "#" type lenghtDef:? ":" text {% assingmentdefType %}
+lenghtDef -> "," _  unsigned_int _ {% nth(2) %}
+
+comment -> "#" text {% d => d[0] + d[1] %}
 
 _ -> wschar:* {% function(d) {return null;} %}
-wschar -> [ \t\v\f] {% id %}
+wschar -> [ \t] {% id %}
  
 transText -> transChar transRest {% d => d[0] + d[1].join("") %}
 transRest -> transChar:* {% id %}
 
 transChar -> [^\n\t\v\f#] {% id %} # for translation text. may not contain #
 
-text -> textChar:* {% id %} #for comments text. may contain spaces and tabs
+text -> textChar:* {% d => d[0].join("") %} #for comments text. may contain spaces and tabs
 textChar -> [^\n\v\f] {% id %}
-word -> char:+ {% d => d[0].join("") %} # for key may not have spaces or whitespace
+key -> char:+ {% d => d[0].join("") %} # for key may not have spaces or whitespace
 char -> [^\n\t\v\f ] {% id %}
+
+type -> "XBUT" | "XFLD"
